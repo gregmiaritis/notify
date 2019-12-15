b78242620700ee6a697b54134c936be849f75459
Miaritis Greg <gregmiaritis@aol.com>
2019-12-15 20:11:04 +0000

    SlackController

--- app/Http/Controllers/SlackUsersController.php
+++ app/Http/Controllers/SlackUsersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use SlackHelper;
 use App\SlackUser;
 use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +10,7 @@ class SlackUsersController extends Controller
 {
     private $slack_api_link;
     private $slack_api_token;
-    private $slack_bot_user_token;
     private $slack_api_token_type;
-    private $slack_post_message_endpoint;
     private $slack_users_endpoint;
 
     public function __construct()
@@ -22,8 +19,6 @@ class SlackUsersController extends Controller
         $this->slack_api_token = config('services.slack.token');
         $this->slack_api_token_type = config('services.slack.token_type');
         $this->slack_users_endpoint = config('services.slack.users_endpoint');
-        $this->slack_bot_user_token = config('services.slack.bot_user_token');
-        $this->slack_post_message_endpoint = config('services.slack.post_message_endpoint');
     }
 
     public function importSlackUsers()
@@ -57,21 +52,4 @@ class SlackUsersController extends Controller
         return response()->json('Imported', Response::HTTP_OK);
     }
 
-    public function sendSlackMessage()
-    {
-        $slack_user = SlackUser::find(4); //make it dynamic
-
-        if (!$slack_user->private_channel_id) {
-            $slack_user = SlackHelper::getPrivateChannelId($slack_user->id);
-        }
-
-        $client = new Client;
-        $client->post(
-            $this->slack_api_link.
-            $this->slack_post_message_endpoint.
-            SlackHelper::sendSlackMessage($this->slack_bot_user_token, $slack_user)
-        );
-
-        return response()->json('Message has been sent', Response::HTTP_OK);
-    }
 }
