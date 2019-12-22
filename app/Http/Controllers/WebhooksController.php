477efa6c1645bc8bf6f38ff58d85ae9f0ea8023b
Miaritis Greg <gregmiaritis@aol.com>
2019-12-23 01:09:18 +0200

    github functon in WebhooksController

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 class WebhooksController extends Controller
 {
     const OPENED_PR = 'opened';
+    const OPEN_PR_GITHUB = 'open';
     
     public function gitlab(Request $request)
     {
@@ -19,7 +20,7 @@ class WebhooksController extends Controller
         $author = SlackHelper::prepareData($request->user);
         $pull_request = SlackHelper::prepareData($request->object_attributes);
 
-        if($pull_request->state === SlackUsersController::OPENED_PR) {
+        if($pull_request->state === WebhooksController::OPENED_PR) {
             $pull_request_data = SlackHelper::gitlabData($pull_request, $author);
             SlackHelper::sendSlackMessage($slack_user, $pull_request_data);
         }
@@ -35,6 +36,20 @@ class WebhooksController extends Controller
         
         SlackHelper::sendSlackMessage($slack_user, $pull_request_data);
         
+        return new JsonResponse(['message' => 'Message has been sent'], Response::HTTP_OK);
+    }
+    
+    public function github(Request $request)
+    {
+        $pull_request = json_decode($request->payload);
+
+        $slack_user = SlackUser::find(4); //make it dynamic
+
+        if($pull_request->pull_request->state === WebhooksController::OPEN_PR_GITHUB) {
+            $pull_request_data = SlackHelper::githubData($pull_request->pull_request);
+            SlackHelper::sendSlackMessage($slack_user, $pull_request_data);
+        }
+
         return new JsonResponse(['message' => 'Message has been sent'], Response::HTTP_OK);
     }
 }
